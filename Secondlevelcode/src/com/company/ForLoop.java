992069378e91894bd48c77bb6090d5e5e84b1dce
Rohan <rgupta6_be20@thapar.edu>
2021-08-09 14:13:00 +0545

    Added for and while loop

--- Secondlevelcode/src/com/company/ForLoop.java
+++ Secondlevelcode/src/com/company/ForLoop.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 public class ForLoop {
     public static void main(String[] args) {
+        /*
+        Syntax of for loop
+        for(initialization; condition, change){
+            //body
+        }
+         */
+
+        // Simple Program to print numbers from 1 to 9
         System.out.print("Enter any number: ");
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
         for(int n=1; n<num; n++){
             System.out.println(n);
         }
-
+        // Use while when you dont know the number of iterations, use for loop when you know the number of iterations
     }
 }
---
+++ Secondlevelcode/src/com/company/Whileloop.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Whileloop {
+    public static void main(String[] args){
+        /*
+        Syntax of while loop:
+        while(condition){
+            //body
+        }
+         */
+
+        //Simple program printing numbers from 0 to 9 using while loop
+
+        int num = 1;
+        while(num<10){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
