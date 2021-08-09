089a82cc1f73cd63a89b8e387cf2dc4cd74b4e9c
Rohan <rgupta6_be20@thapar.edu>
2021-08-09 14:19:56 +0545

    Added simple programs for conditional statements and loops

---
+++ Secondlevelcode/src/com/company/dowhileloop.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class dowhileloop {
+    public static void main(String[] args){
+        /*
+        Syntax of do while loop
+        do{
+            //body
+        }while(condition);
+         */
+        int num = 1;
+        do {
+            System.out.println(num);
+            num++;
+        }while(num<10);
+    }
+}
