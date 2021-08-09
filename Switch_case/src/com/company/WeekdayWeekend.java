e873c963671f06880ef1bda6e6d4db4b1a0666dc
Rohan <rgupta6_be20@thapar.edu>
2021-08-09 21:38:17 +0545

    Switch case

---
+++ Switch_case/src/com/company/WeekdayWeekend.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class WeekdayWeekend {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        switch(num){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+        }
+    }
+}
