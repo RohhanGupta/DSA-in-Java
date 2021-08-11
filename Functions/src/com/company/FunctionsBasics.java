5486d006ed5685dcb4c0a834017f5c9a499e9c9c
Rohan <rgupta6_be20@thapar.edu>
2021-08-11 12:41:28 +0545

    Functions

--- Functions/src/com/company/FunctionsBasics.java
+++ Functions/src/com/company/FunctionsBasics.java
@@ -6,8 +6,10 @@ public class FunctionsBasics {
 
     public static void main(String[] args) {
 	    sum();
+	    int ans = sum2();
+        System.out.println(ans);
     }
-
+    //Function without parameters and return
     static void sum()  {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your first number: ");
@@ -16,5 +18,16 @@ public class FunctionsBasics {
         int num2 = input.nextInt();
         int sum = num1+num2;
         System.out.println("The sum is: "+ sum);
+    }
+
+    //Function without parameters but return
+    static  int sum2() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter your second number: ");
+        int num2 = input.nextInt();
+        int sum = num1+num2;
+        return sum;
     }
 }
