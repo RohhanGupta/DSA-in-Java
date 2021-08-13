7fd5b288974962a093db382edb7391e90b32a37a
Rohan <rgupta6_be20@thapar.edu>
2021-08-13 14:34:51 +0545

    Arrays

--- Arrays/src/com/company/Arrays.java
+++ Arrays/src/com/company/Arrays.java
@@ -2,6 +2,18 @@ package com.company;
 
 public class Arrays {
     public static void main(String[] args) {
-	    
+        //Q1: Store 5 roll numbers
+        // Array syntax: datatype[] variable_name = new datatype[size];
+        int[] rollnumbers = new int[5];
+        //Or directly
+        int[] rollnumbers2 = {23,23,53,52,51};
+        System.out.println(rollnumbers2[2]);
+
+        //declaration of array, roll number is defined in the stack
+        // Initialization: Actually here object is being created in the heap
+
+        System.out.println(rollnumbers[1]);
+        //This will give us 0 as this is the default value
+
     }
 }
