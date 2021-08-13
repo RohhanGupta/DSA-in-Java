97646b28ae9a0fc693e54069c45e10785d1b6c54
Rohan <rgupta6_be20@thapar.edu>
2021-08-13 12:30:21 +0545

    Functions

--- Functions/src/com/company/FunctionOverloading.java
+++ Functions/src/com/company/FunctionOverloading.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class FunctionOverloading {
+    public static void main(String[] args) {
+        /*
+        Function overloading happens when two functions with the same name but different arguments is declared
+         */
+        func(2,3);
+        func(4,5,6);
+    }
+
+    static void func(int a, int b){
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    static void func(int a, int b, int c){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
 }
