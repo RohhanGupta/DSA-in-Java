9e8ae7df0081b0102c541b7a440dd8b5914b59de
Rohan <rgupta6_be20@thapar.edu>
2021-08-17 13:26:02 +0545

    Max number in 2D array

--- LinearSearch/src/com/company/MaxTdArray.java
+++ LinearSearch/src/com/company/MaxTdArray.java
@@ -2,6 +2,20 @@ package com.company;
 
 public class MaxTdArray {
     public static void main(String[] args) {
-        
+        int[][] arr = {
+                {-4,2,3},
+                {9,4,-55},
+                {8,7,6}
+        };
+
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                if (anInt > max) {
+                    max = anInt;
+                }
+            }
+        }
+        System.out.println(max);
     }
 }
