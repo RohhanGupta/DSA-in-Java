bb22f9305d59ba8a2215429a5a1ed1125fd571de
Rohan <rgupta6_be20@thapar.edu>
2021-08-24 13:09:55 +0545

    Bubble Sort

---
+++ Sorting/src/com/company/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+	    int[] arr = {1,432,53,75,24};
+	    bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int arr[]){
+        for(int i =0; i<arr.length; i++){
+            boolean swapped = false;
+            for(int j = 1; j<arr.length-i; j++){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped==false){
+                break;
+            }
+        }
+    }
+
+}
